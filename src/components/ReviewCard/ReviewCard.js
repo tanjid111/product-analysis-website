2cf17a4280960d6f592ad63f6fa882d181e3d53d
tanjid111 <tanjidhossain@gmail.com>
2022-04-03 22:34:01 -0700

    netlify fix updated

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './ReviewCard.css'
+import Rating from 'react-rating';
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ReviewCard = (props) => {
-    const { review, picture, name } = props.review;
+    const { review, picture, name, rating } = props.review;
     return (
         <div className='border-solid border-2 my-3 py-5 rounded-2xl w-3/5 mx-auto shadow-lg'>
-            <div className='review flex justify-center items-center py-5'>
+            <div className='review flex justify-center items-center pt-5'>
                 <img src={picture} alt="" />
                 <p className='px-5 text-3xl'>{name}</p>
             </div>
+            <div className='text-center'>
+                <Rating
+                    initialRating={rating}
+                    emptySymbol={<FontAwesomeIcon icon={faStar} />}
+                    fullSymbol={<FontAwesomeIcon style={{ color: 'goldenrod' }} icon={faStar} />}
+                    readonly
+                ></Rating>
+            </div>
+
 
             <p className='text-center px-12 text-xl'>{review}</p>
 
